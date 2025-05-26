77517ac5b52bb4562b3bff0da535a7d5e7166702
Mauro <maurodefalco96@gmail.com>
2025-05-27 00:31:55 +0200

    install tailwind and supabase

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold">Nuxt Shortner</h1>
   </div>
 </template>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase']
 })
