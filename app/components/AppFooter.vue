ace4250d3f4a7f3a491f3dbf506763bf30708129
Mauro <maurodefalco96@gmail.com>
2025-05-31 00:05:00 +0200

    sign up btn

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class= text-white text-center">
+  <footer class="text-white text-center">
     <div class="container mx-auto py-4 flex justify-center items-center">
       <div class="text-sm">
         &copy; {{ new Date().getFullYear() }} Nuxt Shortner
--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -9,22 +9,22 @@
   const links: Links[] = [
     { name: 'Home', href: '/', id: 1},
     { name: 'Login', href: '/login', id: 2},
-    { name: 'Register', href: '/register', id: 3},
     { name: 'Dashboard', href: '/dashboard', id: 4},
   ];
 
 </script>
 
 <template>
-  <div class="max-w-[1280px] mx-auto p-4 flex justify-between items-center">
+  <div class="max-w-[1280px] border-b mx-auto p-4 flex justify-between items-center">
     <NuxtLink to="/"><h5 class="text-2xl font-bold">Nuxt Shortner</h5></NuxtLink>
     <nav>
-      <ul class="flex space-x-4">
+      <ul class="flex space-x-4 items-center">
         <li v-for="link in links" :key="link.id">
           <NuxtLink :to="link.href" class="hover:underline"> 
             {{ link.name }}
           </NuxtLink>
         </li>
+        <NuxtLink to="/register" class="btn">Register</NuxtLink>
       </ul>
     </nav>
   </div>
