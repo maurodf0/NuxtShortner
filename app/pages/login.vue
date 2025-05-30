f324bc7c6d19b1861f225393aa3240ca9b58bee8
Mauro <maurodefalco96@gmail.com>
2025-05-31 00:53:02 +0200

    fix: correct NuxtLink destination from /dashboard to /dashboard in Hero component

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -3,5 +3,14 @@
 </script>
 
 <template>
+  <section class="h-screen grid place-content-center">
+    <div class="container"> 
 
+     <div class="card">
+      <button class="btn-primary py-5 w-full"> Continue with Github</button>
+     </div>
+     
+    </div>
+
+  </section>
 </template>
