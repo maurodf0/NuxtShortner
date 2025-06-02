7a2dc1365e15417c553e1accf87672cc84f9369b
Mauro <maurodefalco96@gmail.com>
2025-06-02 20:36:13 +0200

    url init

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -3,15 +3,20 @@
 
 <template>
 <main>
+  <section class="container mt-4">
+    <h1 class="text-4xl font-bold mb-4">Dashboard</h1>
+  </section>
   <section class="container">
-    <h1>Dashboard</h1>
+    <div class="card">
+      <form>
+        <label for="url" class="block mb-4 text-sm font-medium text-gray-300">Enter URL to shorten</label>
+        <input type="text" class="input border p-2 rounded border-white/10 w-full mb-4"placeholder="Enter URL to shorten">
+      </form>
+    </div>
   </section>
-  <section>
 
-  </section>
+  <section class="container">
 
-  <section>
-    
   </section>
 </main>
 </template>
