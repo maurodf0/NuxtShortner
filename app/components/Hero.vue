163002c2c6e488adf633415bf204a1002772ce8e
Mauro <maurodefalco96@gmail.com>
2025-06-02 19:37:33 +0200

    fix height

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="h-screen grid place-content-center">
+<section class="h-[550px] grid place-content-center">
   <div class="text-center max-w-2xl mx-auto px-4">
     <h1 class="text-6xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-b from-white to-gray-400">Make your URL Shine with Nuxt Shortner</h1>
     <p class="text-lg mb-8">Your go-to solution for shortening URLs effortlessly.</p>
