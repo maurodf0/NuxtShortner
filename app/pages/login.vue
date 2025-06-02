d139a835844f568226fc12f145919fa53e906438
Mauro <maurodefalco96@gmail.com>
2025-06-02 20:08:24 +0200

    refactor: update button styles and improve form layout in login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -7,8 +7,30 @@
     <div class="container"> 
 
      <div class="card">
-      <button class="btn-primary py-5 w-full"> Continue with Github</button>
+      <button class="btn py-5 w-full rounded-full"> 
+        Continue with Github
+      </button>
+
+
+     <hr class="border-white/15 my-6">
+     <form>
+      <div>
+        <label for="email" class="block mb-2 text-sm  font-medium text-gray-300">Email</label>
+        <input type="email" id="email" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter your email">
+      </div>
+      <div>
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-300">Password</label>
+        <input type="password" id="password" class="input w-full border p-2 border-white/10 rounded mb-4" placeholder="Enter your Password">
+      </div>
+      <button type="submit" class="btn w-full py-3 rounded-full">Login</button>
+      <p class="text-sm text-gray-400 mt-4">
+        Don't have an account? 
+        <NuxtLink to="/register" class="text-white hover:underline">Register</NuxtLink>
+      </p>
+     </form>
+
      </div>
+
      
     </div>
 
