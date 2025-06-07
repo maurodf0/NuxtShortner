ac2a9041ec69a4e60007d235e2762c10e9d9fb9d
Mauro <maurodefalco96@gmail.com>
2025-06-07 19:10:21 +0200

    refactor: improve form layout and styling in dashboard

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -8,9 +8,16 @@
   </section>
   <section class="container">
     <div class="card">
-      <form>
-        <label for="url" class="block mb-4 text-sm font-medium text-gray-300">Enter URL to shorten</label>
-        <input type="text" class="input border p-2 rounded border-white/10 w-full mb-4"placeholder="Enter URL to shorten">
+      <form class="flex items-center gap-5">
+        <div class="w-5/12">
+        <label for="url" class="block mb-2 text-sm font-medium text-gray-300">Enter URL to shorten</label>
+        <input type="text" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter URL to shorten">
+      </div>
+      <div class="w-5/12">
+        <label class="block mb-2 text-sm font-medium text-gray-300" for="shortKey" >Short Key</label>
+        <input type="text" id="shortkey" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Short Key">
+      </div>
+      <button class="w-2/12 mt-[10px] btn btn-primary">Shorten</button>
       </form>
     </div>
   </section>
