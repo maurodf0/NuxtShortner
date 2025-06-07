eb4c17d92979f73d00908d2b8a078a0009d9842e
Mauro <maurodefalco96@gmail.com>
2025-06-07 19:14:14 +0200

    create LinkForm components

---
+++ app/components/LinkForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+      <div class="card">
+      <form class="flex items-center gap-5">
+        <div class="w-5/12">
+        <label for="url" class="block mb-2 text-sm font-medium text-gray-300">Enter URL to shorten</label>
+        <input type="text" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter URL to shorten">
+      </div>
+      <div class="w-5/12">
+        <label class="block mb-2 text-sm font-medium text-gray-300" for="shortKey" >Short Key</label>
+        <input type="text" id="shortkey" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Short Key">
+      </div>
+      <button class="w-2/12 mt-[10px] btn btn-primary">Shorten</button>
+      </form>
+    </div>
+</template>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -7,19 +7,7 @@
     <h1 class="text-4xl font-bold mb-4">Dashboard</h1>
   </section>
   <section class="container">
-    <div class="card">
-      <form class="flex items-center gap-5">
-        <div class="w-5/12">
-        <label for="url" class="block mb-2 text-sm font-medium text-gray-300">Enter URL to shorten</label>
-        <input type="text" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter URL to shorten">
-      </div>
-      <div class="w-5/12">
-        <label class="block mb-2 text-sm font-medium text-gray-300" for="shortKey" >Short Key</label>
-        <input type="text" id="shortkey" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Short Key">
-      </div>
-      <button class="w-2/12 mt-[10px] btn btn-primary">Shorten</button>
-      </form>
-    </div>
+    <LinkForm />
   </section>
 
   <section class="container">
