19b9d2cec88093e90edbea5604bd39c252247366
Mauro <maurodefalco96@gmail.com>
2025-06-10 00:39:13 +0200

    env fuck

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,11 +2,22 @@
 
 const supabase = useSupabaseClient()
 
+const mail = ref('');
+const psw = ref('');
+
+ const signIn = async (email, password) => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+  if (error) console.log(error)
+}
+
 const signInWithOAuth = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
-      redirectTo: '/dashboard',
+      redirectTo: 'http://localhost:3000/dashboard' 
     },
   })
   if (error) console.log(error)
@@ -29,14 +40,14 @@ const signOut = async () => {
 
 
      <hr class="border-white/15 my-6">
-     <form>
+     <form @submit.prevent="signIn">
       <div>
-        <label for="email" class="block mb-2 text-sm  font-medium text-gray-300">Email</label>
-        <input type="email" id="email" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter your email">
+        <label  for="email" class="block mb-2 text-sm  font-medium text-gray-300">Email</label>
+        <input v-model="mail" type="email" id="email" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter your email">
       </div>
       <div>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-300">Password</label>
-        <input type="password" id="password" class="input w-full border p-2 border-white/10 rounded mb-4" placeholder="Enter your Password">
+        <label  for="password" class="block mb-2 text-sm font-medium text-gray-300">Password</label>
+        <input v-model="psw" type="password" id="password" class="input w-full border p-2 border-white/10 rounded mb-4" placeholder="Enter your Password">
       </div>
       <button type="submit" class="btn w-full py-3 rounded-full">Login</button>
       <p class="text-sm text-gray-400 mt-4">
