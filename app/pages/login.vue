4e6d9831cd0a56d62c56041011da96e598c4acc2
Mauro <maurodefalco96@gmail.com>
2025-06-14 19:33:01 +0200

    feat: implement user registration and enhance login functionality in login.vue

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -5,6 +5,12 @@ const supabase = useSupabaseClient()
 const email = ref<string>('');
 const password = ref<string>('');
 const error = ref<string | null>(null)
+const login = ref<boolean>(true);
+const message = ref<string | null>(null)
+
+watch(login, () => {
+  console.log('Login:', login.value)
+})
 
  const signIn = async () => {
   error.value = null;
@@ -32,6 +38,20 @@ const signInWithOAuth = async () => {
   if (error) console.log(error)
 }
 
+const signUp = async () => {
+  message.value = null;
+  const { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+  })
+  if (error) console.log(error)
+
+  if (data) {
+    message.value = 'Account created successfully! Please confirm your email to activate your account.'
+  }
+
+}
+
 
 </script>
 
@@ -39,6 +59,7 @@ const signInWithOAuth = async () => {
   <section class="h-screen grid place-content-center">
     <div class="container"> 
 
+   
      <div class="card">
       <button @click="signInWithOAuth" class="btn py-5 w-full rounded-full"> 
         Continue with Github
@@ -46,9 +67,11 @@ const signInWithOAuth = async () => {
 
 
      <hr class="border-white/15 my-6">
+     <div v-if="login">
+
      <form @submit.prevent="signIn">
       <div>
-        <label  for="email" class="block mb-2 text-sm  font-medium text-gray-300">Email</label>
+        <label  for="email" class="block mb-2 text-sm  font- medium text-gray-300">Email</label>
         <input v-model="email" type="email" id="email" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter your email">
       </div>
       <div>
@@ -58,16 +81,45 @@ const signInWithOAuth = async () => {
       <button type="submit" class="btn w-full py-3 rounded-full">Login</button>
       <p class="text-sm text-gray-400 mt-4">
         Don't have an account? 
-        <NuxtLink to="/register" class="text-white hover:underline">Register</NuxtLink>
+        <span @click="login = !login" class="text-center text-white hover:underline">Register</span>
       </p>
-     </form>
+     </form>  
+
+    </div>
+
+    <div v-else>
+      <form @submit.prevent="signUp">
+      <div>
+        <label for="email" class="block mb-2 text-sm  font- medium text-gray-300">Email</label>
+        <input v-model="email" type="email" id="email" class="input border p-2 rounded border-white/10 w-full mb-4" placeholder="Enter your email">
+      </div>
+      <div>
+        <label  for="password" class="block mb-2 text-sm font-medium text-gray-300">Password</label>
+        <input v-model="password" type="password" id="password" class="input w-full border p-2 border-white/10 rounded mb-4" placeholder="Enter your Password">
+      </div>
+      <button type="submit" class="btn w-full py-3 rounded-full">Register</button>
+      <p class="text-sm text-gray-400 mt-4">
+       Already have an account? 
+        <span @click="login = !login" class="text-center text-white hover:underline">Login</span>
+      </p>
+     </form>  
+
+      </div>
+
      <div v-if="error" class="text-red-500 mt-4">
        <strong>Error:</strong> 
-       <!-- Display the error message --> 
+
       {{ error }}
      </div>
 
+     <div v-if="message" class="mt-4">
+       <strong>Info:</strong> 
+
+      {{ message }}
      </div>
+
+     </div>
+     
 
      
     </div>
--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -19,11 +19,11 @@ const signOut = async () => {
     <p class="text-gray-400">This is your profile page.</p>
     <p class="text-gray-400">You can add more profile features here.</p>
   </div>
-  <div class="container mt-10">
-    <h2 class="text-2xl font-bold mb-4">User Information</h2>
+  <div class="container mt-10 ">
+    <h2 class="text-2xl font-bold mb-4 ">User Information</h2>
     <p class="text-gray-400">Name: {{ user?.user_metadata.full_name || 'N/A' }}</p>
     <p class="text-gray-400">Email: {{ user?.email || 'N/A' }}</p>
-    <p class="text-gray-400">Avatar: <img :src="user?.user_metadata.avatar_url" alt="User Avatar" class="w-24 h-24 rounded-full"></p>
+  
     <button @click="signOut" class="btn btn-primary mt-4">Sign Out</button>
   </div>
 
