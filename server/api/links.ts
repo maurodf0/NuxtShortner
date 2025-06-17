be7905ded9f4f534ca63ff58f02ca037e6830c13
Mauro <maurodefalco96@gmail.com>
2025-06-17 13:08:44 +0200

    fix: correct property name from 'url' to 'longUrl' in links API response

--- server/api/links.ts
+++ server/api/links.ts
@@ -1,12 +1,11 @@
 export default defineEventHandler(async (event) => {
-
   return [{
     id: 1,
     shortKey: 'NSL2',
-    url: 'https://nuxtshortener.vercel.app'
+    longUrl: 'https://nuxtshortener.vercel.app'
   }, {
     id: 2,
     shortKey: '/2',
-    url: 'https://nuxtshortener.vercel.app'
+    longUrl: 'https://nuxtshortener.vercel.app'
   }]
 })
