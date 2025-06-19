349d47038a2edf72a2398725d0236a1ecbbf7393
Mauro <maurodefalco96@gmail.com>
2025-06-20 00:34:45 +0200

    fix: remove unnecessary redirect options from signInWithOAuth function

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -30,9 +30,7 @@ watch(login, () => {
 const signInWithOAuth = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
-    options: {
-      redirectTo: 'http://localhost:3000/dashboard' 
-    },
+    
   })
   if (error) console.log(error)
 }
