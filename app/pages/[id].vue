26acfb4e9ebb36c131233dc023e420bd7762bcf0
Mauro <maurodefalco96@gmail.com>
2025-06-19 23:04:25 +0200

    fix: fetch and display link data in [id].vue component

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
 const id = useRoute().params.id
+const supabase = useSupabaseClient();
+
+
+
+//take data from Supa
+const { data } = await useAsyncData('link', async () => {
+  const { data, error } = await supabase
+  .from('links')
+  .select('*')
+  .eq('key', id)
+  .single()
+
+  console.log(data)
+
+
+  return data
+})
+
+//Redirect to long url
 </script>
 
 <template>
-  {{ id }}
+  {{ id }} <br>
+  {{ data }}
 </template>
